9320b0e3f49494a8618facfe6178ac45dbee6446
Miguel <63415435+mruiz99@users.noreply.github.com>
2021-03-04 10:19:51 -0500

    notes

---
+++ script.js
@@ -0,0 +1,7 @@
+fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json')
+  .then(data) => data.json())
+  .then((data2) => {
+      // do something with your data
+
+      form.addEventListener
+  })
